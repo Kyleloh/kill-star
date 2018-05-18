ece3d9adedf2296abcfd52a793cd7cbc7004ffb1
kyle.lo <kyle.lo@vipshop.com>
2018-05-18 20:13:48 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const COLORS = ['#020d34', '#cf5a5a', '#c7bd45', '#6897d1', '#73d45c', '#e15dec'];
 
+let gameover = false;
+
 // 根据图片生成方块
 function getDataFromPic() {
     return new Promise((resolve, reject) => {
-        // const canvas = document.querySelector('#canvas');
         const canvas = document.createElement('canvas');
         canvas.width = 750;
         canvas.height = 1334;
@@ -18,7 +19,7 @@ function getDataFromPic() {
             let rows = new Array(10).fill(new Array(10).fill(0));
             rows = rows.map((item, rowIndex) => {
                 return item.map((item, colIndex) => {
-                    let a = ctx.getImageData(55+rowIndex*75, 1155-colIndex*75, 1, 1).data;
+                    let a = ctx.getImageData(55 + rowIndex * 75, 1155 - colIndex * 75, 1, 1).data;
 
                     let r = a[0],
                         g = a[1],
@@ -41,94 +42,118 @@ function getDataFromPic() {
     })
 }
 
+
+// 1 => 1000
+// 2 => 3000
+// 3 => 6000
+// 4 => 8000
+// 5 => 10000
+// 6 => 13000
+// 7 => 15000
+// ...
+// 34 => 120000
+// 35 => 124000
+
+
 function initGame(rows) {
     new Vue({
         el: "#box",
         data: {
             rows: rows,
             colors: COLORS,
-            // colors: ['white', 'red', 'yellow', 'blue', 'green', 'pink'],
             selected: [''],
-            score: 0
+            score: 0,
+            remainingParts: [],
+            blockAmount: 100,
         },
         created() {
-            // const rows = this.$data.rows;
-            // while (rows.length < 10) {
-            //     const arr = [];
-            //     while (arr.length < 10) {
-            //         arr.push(parseInt(Math.random() * 5) + 1)
-            //     }
-            //     rows.push(arr);
-            // }
-            // this.$data.rows = [
-            //     [1, 5, 5, 5, 2, 2, 1, 2, 1, 1],
-            //     [5, 1, 5, 5, 3, 5, 2, 2, 2, 3],
-            //     [4, 1, 3, 4, 4, 4, 4, 2, 2, 3],
-            //     [4, 5, 5, 2, 4, 1, 1, 1, 4, 1],
-            //     [5, 3, 3, 1, 1, 5, 4, 2, 2, 3],
-            //     [2, 3, 3, 3, 2, 2, 3, 3, 1, 4],
-            //     [4, 3, 1, 5, 2, 1, 5, 4, 4, 4],
-            //     [1, 3, 4, 3, 3, 3, 2, 4, 2, 1],
-            //     [1, 3, 3, 3, 3, 5, 2, 4, 1, 4],
-            //     [5, 4, 2, 2, 2, 2, 2, 4, 2, 2],
-            // ];
+            this.resetRemainingParts();
         },
         methods: {
-
+            resetRemainingParts() {
+                let remainingParts = [];
+                this.$data.rows.forEach((item, rowIndex) => {
+                    item.forEach((item, colIndex) => {
+                        const sameColorBlock = this._getSameColorBlock(rowIndex, colIndex);
+                        if (sameColorBlock.length > 1 && !remainingParts.some(item => item.indexOf(`${rowIndex},${colIndex}`) != -1)) {
+                            const partString = sameColorBlock.join(';');
+                            remainingParts.push(partString);
+                        }
+                    })
+                });
+                this.$data.remainingParts = remainingParts;
+            },
             clickHandler(x, y) {
-                // console.log('点击');
                 const key = `${x},${y}`;
                 const value = this.$data.rows[x][y];
                 if (value == 0) {
-                    // console.log('0，无效');
                     return
                 };
                 if (this.$data.selected.indexOf(key) == -1) {
-
-                    // console.log('选择');
                     this.$data.selected = [];
-                    this._selectBlock(x, y, value);
+                    this._selectBlock(x, y);
                     if (this.selected.length == 1) {
                         this.selected.length = 0;
                     }
                 } else {
-                    // console.log('清除');
-                    this._distroyBlock();
                     console.log('清除点：', x, y);
+                    this._distroyBlock();
                 }
             },
             _score(num) {
                 return num > 1 ? num * num * 5 : 0;
             },
-            _selectBlock(x, y, val) {
+            _selectBlock(x, y) {
+                let selectArray = this._getSameColorBlock(x, y);
+                if (selectArray.length < 2) return;
+                this.$data.selected = selectArray;
+            },
+            _getSameColorBlock(x, y, val, result = []) {
                 const key = `${x},${y}`;
                 const value = this.$data.rows[x] && this.$data.rows[x][y];
-                if (value && value == val && this.$data.selected.indexOf(key) == -1) {
-                    this.$data.selected.push(key);
-                    this._selectBlock(x - 1, y, value);
-                    this._selectBlock(x + 1, y, value);
-                    this._selectBlock(x, y - 1, value);
-                    this._selectBlock(x, y + 1, value);
+                if (!val || (value && value == val && result.indexOf(key) == -1)) {
+                    result.push(key);
+                    result = result.concat(this._getSameColorBlock(x - 1, y, value, result));
+                    result = result.concat(this._getSameColorBlock(x + 1, y, value, result));
+                    result = result.concat(this._getSameColorBlock(x, y - 1, value, result));
+                    result = result.concat(this._getSameColorBlock(x, y + 1, value, result));
                 }
+                return [...new Set(result)];
             },
             _distroyBlock() {
                 if (this.selected.length < 2) return;
-                // console.log('清除内容', this.$data.selected);
+                console.log(this.selected[0]);
                 this.$data.selected.forEach(item => {
                     const xy = item.split(',');
                     const x = xy[0];
                     const y = xy[1];
                     this.$data.rows[x].splice(y, 1, 0);
                 });
-                this.$data.score += this._score(this.$data.selected.length);
-                this.$data.selected = [];
                 this._cleanUpData();
             },
             _cleanUpData() {
                 this.$data.rows = this.$data.rows
                     .filter(item => item.some(item => item != 0))
                     .map(item => item.filter(item => item != 0));
+
+                this.resetRemainingParts();
+
+                this.countScore();
+
+                this.$data.selected = [];
             },
+            countBlock() {
+                this.$data.amount = this.$data.rows.reduce((total, item) => total + item.filter(item => item != 0).length, 0);
+            },
+            countScore() {
+                this.$data.score += this._score(this.$data.selected.length);
+                this.countBlock();
+                if (this.$data.remainingParts.length == 0) {
+                    this.$data.score += Math.max(0, 2000 - this.$data.amount * this.$data.amount * 20);
+                    console.log('game over! score: ' + this.$data.score);
+                    gameover = true;
+                }
+            }
         }
     });
 }
